3842247272b2f4fb4a9ec7c8a4cca66c3cd14a3e
Adi Astawa <adiast457@gmail.com>
2024-05-03 13:20:45 +0800

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#navbar").load("app/components/navbar.html");
+    $("#footer").load("app/components/footer.html");
+    $("#content").load("app/components/content/home.html");
+});
